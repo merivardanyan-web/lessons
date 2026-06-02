608fc8886967ee1c67804517a78fdb9d9c2f56e0
merivardanyan-web <meridigischool@gmail.com>
2026-06-02 16:09:39 +0400

    first lesson

---
+++ js/lessons.js
@@ -0,0 +1,6 @@
+// Lessons common JavaScript
+// Currently minimal - can be extended for navigation, theme switching, etc.
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Lesson page loaded');
+});
